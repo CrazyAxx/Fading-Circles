48a5ea88badf05c06d2ed44735e54e825f3cd38e
Collin Bates <244503@bwls.net>
2016-10-20 11:49:41 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ function setup() {
 }
 
 function draw() {
-  background( 255 , 255 , 255 , 30 )
+  background( 255 , 255 , 255 , 40 )
   ellipse( mouseX , mouseY , 75 , 75 );
   fill( 255 , 2 , 255 );
 }
